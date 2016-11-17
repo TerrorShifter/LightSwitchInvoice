245d979d2fa78054fd4f211574d5ddf42f56c6a4
Jonathan Davis <jonathanedavis@outlook.com>
2016-11-17 01:18:33 -0600

    First Commit
    ..

---
+++ LightSwitchInvoice/LightSwitchInvoice.Server/DataSources/ApplicationData/Customer.lsml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.LightSwitch;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace LightSwitchApplication
+{
+    public partial class Customer
+    {
+        partial void OrderTotal_Compute(ref decimal result)
+        {
+            // Set result to the desired field value
+            result = (from items in Orders select items).Sum(X => X.OrderAmount);
+        }
+    }
+}
